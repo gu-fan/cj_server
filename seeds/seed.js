cd19829cb11cfa2d8f0f7a41b4df40f20f340d9f
gu-fan <gu.fan@qq.com>
2018-12-06 23:30:48 +0800

    -

--- seeds/seed.js
+++ seeds/seed.js
@@ -14,7 +14,7 @@ try {
   console.log(e);
 }
 
-var time = mmt("2018-11-10T00:00:00+08:00")
+var time = mmt("2018-11-20T00:00:00+08:00")
 
 var tmp = mmt(time)
 var times = _.range(30).map(function(idx){
@@ -35,9 +35,9 @@ exports.seed = function(knex, Promise) {
         {id:3, wx_id:'wx_gha9uwfy982hz', name:'alf01j8',created_at: times[3].format()},
         {id:4, wx_id:'wx_a0f9k234f0kjf', name:'君子好逑～',created_at:times[4].format(), avatar:'/static/wx_avatar/4.jpg', total_answer_zhichi: 1,},
         {id:5, wx_id:'wx_zvm8342rjojzo', name:'lmf10z1',created_at:times[5].format()},
-        {id:6, wx_id:'wx_z90fe01091304', name:'傻瓜这是装',created_at:times[6].format(), avatar:'/static/wx_avatar/6.jpg', total_answer_zhichi: 3,},
+        {id:6, wx_id:'wx_z90fe01091304', name:'明明很困',created_at:times[6].format(), avatar:'/static/wx_avatar/6.jpg', total_answer_zhichi: 3,},
         {id:7, wx_id:'wx_kgjrgif0e9fua', name:'Angel、浮熙', created_at:times[7].format(),avatar:'/static/wx_avatar/7.jpg'},
-        {id:8, wx_id:'wx_0fwaflemalej1', name:'知道狗长啥样', created_at:times[8].format(),avatar:'/static/wx_avatar/8.jpg'},
+        {id:8, wx_id:'wx_0fwaflemalej1', name:'知道长啥样', created_at:times[8].format(),avatar:'/static/wx_avatar/8.jpg'},
         {id:9, wx_id:'wx_09aewlfmzaalk1', name:'Rencontre',created_at:times[9].format(), avatar:'/static/wx_avatar/9.jpg', total_answer_zhichi: 4,},
         {id:10, wx_id:'wx_09aewlfmzalk1', name:'单独等待',created_at:times[10].format(), avatar:'/static/wx_avatar/10.jpg'},
         {id:11, wx_id:'wx_1fa8fajzlkjql', name:'张林远', created_at:times[11].format(), avatar:'/static/wx_avatar/11.jpg', total_answer_thanks: 2, }
