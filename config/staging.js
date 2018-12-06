21de0f95fdfcf9ae15dedf4b92211612ef301bf1
gu-fan <gu.fan@qq.com>
2018-12-06 22:49:08 +0800

    add mysql server

--- config/staging.js
+++ config/staging.js
@@ -5,7 +5,8 @@ module.exports = {
   db:{
     client: 'mysql',
     connection: {
-      host : '172.21.0.4',
+      // host : '172.21.0.4',   // beijing
+      host : '172.17.0.14',     // shanghai
       database: 'test',
       user:     'root',
       password: 'lk123456'
