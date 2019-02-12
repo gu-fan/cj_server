c39d10805dff3b2893034e2bb5b870cfab42aa31
gu-fan <gu.fan@qq.com>
2019-02-13 06:01:17 +0800

    -

--- deploy/test.sh
+++ deploy/test.sh
@@ -1,1 +1,2 @@
-ssh fzbb "cd /home/ubuntu/repos/compose/server && git pull && cd .. && docker-compose start --build"
+ssh fzbb "cd /home/ubuntu/repos/compose/server && git pull && cd .. && docker-compose start"
+# ssh fzbb "cd /home/ubuntu/repos/compose/server && git pull && cd .. && docker-compose up --no-start --build && docker-compose start"
