f91ac3c3dffdfa44bad25a353e638870ce236491
gu-fan <gu.fan@qq.com>
2019-03-20 01:57:08 +0800

    -

--- src/app.js
+++ src/app.js
@@ -20,8 +20,9 @@ app.use(cors());
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-console.log(config.tmp_path)
-app.use('/tmp',  express.static(config.tmp_path))
+if (app.get('env')!='development') {
+  app.use('/tmp',  express.static(config.tmp_path))
+}
 app.use(restc.express());
 
 
